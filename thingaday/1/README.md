f5c995ebbeb8d7d22807935c20e7c18641d2027d
Kevin Leutzinger <me@kevinleutzinger.com>
2014-05-30 22:27:51 -0700

    started thingaday

---
+++ thingaday/1/README.md
@@ -0,0 +1,5 @@
+Project 1: [Basic Project Template](http://www.kevinleutzinger.com/thingaday/1)
+
+I made a basic project template javascript file.
+It adds a header that shows the project number on the top of the page.
+I plan on adding some style to this soon.
---
+++ thingaday/js/template.js
@@ -0,0 +1,8 @@
+var url = window.location.pathname.split('/')
+var projectNum = url[url.length-2]
+
+
+document.body.innerHTML += "<h1> Project "+projectNum+"</h1>";
+document.body.innerHTML += "<a href=http://www.github.com/kleutzinger/thingaday/"+projectNum+">view source on github<a><br>";
+
+document.body.innerHTML += "<p>" + projectNum +"</p>";
