e027043b37359249d73378cdcbd7091fabfa62ce
Kevin Leutzinger <me@kevinleutzinger.com>
2014-05-30 22:49:58 -0700

    fixed templates

--- thingaday/js/template.js
+++ thingaday/js/template.js
@@ -1,4 +1,4 @@
 var url = window.location.pathname.split('/')
 var projectNum = url[url.length-2]
 document.body.innerHTML += "<h1> Project "+projectNum+"</h1>";
-document.body.innerHTML += "<a href=http://www.github.com/kleutzinger/thingaday/"+projectNum+">view source on github<a><br>";
+document.body.innerHTML += "<a href=https://github.com/kleutzinger/kleutzinger.github.io/tree/master/thingaday/"+projectNum+">view source on github<a><br>";
