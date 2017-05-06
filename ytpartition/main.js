7803bb5b08e34e03263203103a98af3ce901e4f8
Kevin <me@kevinleutzinger.com>
2017-05-06 04:21:45 -0700

    v0.2 fixed a couple things

--- ytpartition/main.js
+++ ytpartition/main.js
@@ -12,7 +12,10 @@ function getHash(){
     if (autoplay) { hash = hash.substring(1)};
     split = hash.split("+");
     parts = [];
+    lastid = split[0];
     for (i=0;i<split.length-1;i+=3){
+        if(split[i] == "^") {split[i] = lastid;}
+        else {lastid = split[i];}
         p = newPartition(split[i], parseInt(split[i+1]), parseInt(split[i+2]))
         parts.push(p);
     }
@@ -77,15 +80,19 @@ function putHash(){
     p = partitions;
 
     hash_string = "";
+    lastid = "";
     for(i=0;i<p.length;i++){
-        print(i);
-        h = p[i].id + "+" + p[i].start + "+" + p[i].end + "+";
+        if(i>0){lastid = p[i-1].id;}
+        if (p[i].id == lastid){ temp = "^"; }
+        else {temp = p[i].id};
+        h = temp + "+" + p[i].start + "+" + p[i].end + "+";
         hash_string+= h;
     } 
     checked = document.getElementById('autoCheckBox').checked;
     
     if (checked) { hash_string = "!" + hash_string;}
     sendUrl = "http://www.kevinleutzinger.com/ytpartition/#" + hash_string;
+    if (window.location.href.indexOf("local") != -1) {sendUrl = "http://localhost:8000/#" + hash_string;}
     window.prompt("Copy to clipboard: Ctrl+C (Cmd + C on mac)", sendUrl);
     
 }
@@ -110,20 +117,27 @@ function add(n){
 function writePartitions(p){
     $("#partition_list").empty();
     for (i=0;i<p.length;i++){
-        $("#partition_list").append( '<li>' +p[i].start + "-" + p[i].end +  ", "+ p[i].id + '</li>' );
+        $("#partition_list").append( '<li onclick="playSingleClip('+i+');">' +p[i].start + "-" + p[i].end +  ", "+ p[i].id + '</li>' );
     }
 }
 
+function playSingleClip(i){
+    play(partitions.slice(i,i+1));
+}
 
-function play(){
+function play(partitions){
+    if (partitions.length == 0){return;}
     i = 0;
     player.loadVideoById(partitions[0].id, .1, "large")
     player.playVideo();
     player.seekTo(partitions[0].start);
+    function ensurePlaying(){
+    }
     function checkVid(){
         if (i >= partitions.length){
             clearInterval(refreshIntervalId); 
             player.stopVideo();
+            return;
         }
         p = partitions;
         if (player.getCurrentTime() >= p[i].end){
@@ -136,12 +150,7 @@ function play(){
         }
     };
     
-    var refreshIntervalId = setInterval(checkVid, 500);
+    var refreshIntervalId = setInterval(checkVid, 1000);
     
     console.log("DONE!");
 };
-
-
-
-
-
