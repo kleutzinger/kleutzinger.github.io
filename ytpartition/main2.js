24aa061f91c8336c3518df45c25f98e15b8a1508
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2024-07-04 19:23:36 -0400

    remove ytpartition
    this is now located at
    https://clipyt.kevbot.xyz

--- ytpartition/main2.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	videos = getHash();
-	writePartitions(videos, -1);
-	if (url.indexOf("localhost")!=-1){document.body.style.backgroundColor = "palegreen";}
-	$("#addvideoid").on("input", timestampShortcut);
-	$("#customtitle").on("focusin", updateHash);
-	$("#customtitle").on("focusout",updateHash);
-	//~ $("input").focusin(alert);
-});
